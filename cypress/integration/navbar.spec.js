cffc9022162bcf271fec5ec5ec9a8b6f6f0f0513
Tim Remnant <timwebdesign2013@gmail.com>
2019-03-26 16:53:59 +0000

    Adding tests for navbar

--- cypress/integration/navbar.spec.js
+++ cypress/integration/navbar.spec.js
@@ -11,6 +11,41 @@ describe("Does the navigation bar does as expected", () => {
 
     cy.visit('/');
 
+    cy.get('[data-cy="dark-button"]')
+      .click();
+
+    cy.get('body')
+      .should('have.class', 'dark-mode');
+
+  });
+
+  it ('Should allow the application to be switched to light mode from dark mode', () => {
+
+    cy.visit('/');
+
+    cy.get('[data-cy="dark-button"]').click();
+
+    cy.get('[data-cy="light-button"]').click();
+
+    cy.get('body')
+      .should('have.class', 'light-mode');
+
+  });
+
+  it ('Should allow us to wipe the data', () => {
+
+    cy.visit('/');
+
+    cy.visit('seed/100');
+
+    cy.get('[data-cy="wipe-data"]').click();
+
+    cy.get('[data-cy="incomplete-list-holder"]')
+      .should('not.exist');
+
+    cy.get('[data-cy="complete-list-holder"]')
+      .should('not.exist');
+
   });
 
 });
