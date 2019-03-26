bb7f4efc91830063dc26297b417ea6bcef3d67fa
Tim Remnant <timwebdesign2013@gmail.com>
2019-03-26 09:34:17 +0000

    Nearly completed incomplete spec

--- cypress/integration/home/incomplete-item.spec.js
+++ cypress/integration/home/incomplete-item.spec.js
@@ -11,10 +11,64 @@ describe('All features of incomplete items', () => {
     cy.visit('/');
 
 
-    cy.get('[data-cy="new-item"]').type('some text');
+    cy.get('[data-cy="new-item"]').type('some text').then(() => {
 
-    cy.get();
+      cy.get('[data-cy="new-item-help-text"]');
+
+    });
 
   });
+
+  it ('should show an error message if the incomplete item text is empty', () => {
+
+    cy.visit('/');
+
+    cy.get('[data-cy="new-item"]').trigger('keydown', { keyCode: 13, which: 13 });
+
+    cy.get('[data-cy="error-message-container"]');
+
+  });
+
+  it ('should add an incomplete item', () => {
+
+    cy.visit('/');
+
+    cy.get('[data-cy="new-item"]')
+      .type('Clean the house')
+      .should('have.value','Clean the house')
+      .trigger('keydown', { keyCode: 13, which: 13 });
+
+    cy.get('[data-cy="incomplete-list-holder"]').children().should('have.length', 1);
+
+  });
+
+  it ('should show an error message if duplicate items are trying to be added', () => {
+
+    cy.visit('/');
+
+    for (let i = 0; i < 2; i++) {
+      cy.get('[data-cy="new-item"]')
+      .type('Clean the house')
+      .should('have.value', 'Clean the house')
+      .trigger('keydown', {keyCode: 13, which: 13});
+    }
+
+    cy.get('[data-cy="error-message-container"]');
+
+  });
+
+  it ('should show an error message if a duplicate complete item is being added', () => {
+
+    cy.visit('/');
+
+    cy.get('[data-cy="new-item"]')
+    .type('Clean the house')
+    .should('have.value','Clean the house')
+    .trigger('keydown', { keyCode: 13, which: 13 });
+
+    cy.get('[data-cy="incomplete-list-holder"]').children().first().find('')
+
+  });
+
 
 });
