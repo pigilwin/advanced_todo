267ce60d65656c3504322614cf5148964b38a1b4
Tim Remnant <timwebdesign2013@gmail.com>
2019-03-31 18:07:57 +0100

    Adding network tests

--- cypress/integration/static/network.spec.js
+++ cypress/integration/static/network.spec.js
@@ -4,10 +4,26 @@ describe('Intercept the network requests', () => {
 
     cy.server();
 
-    cy.route('https://my-json-server.typicode.com/pigilwin/fakedbserver/todos', []);
+    cy.route('https://my-json-server.typicode.com/pigilwin/fakedbserver/todos', [{
+      "id": "001",
+      "value": "Battle",
+      "priority": "L",
+      "complete": "N"
+    }]);
 
     cy.visit('/static');
 
   });
 
+  it ('Should take control of the network using a fixture', () => {
+
+    cy.fixture('items.json').as('itemsJson');
+
+    cy.server();
+
+    cy.route('https://my-json-server.typicode.com/pigilwin/fakedbserver/todos', '@itemsJson');
+
+    cy.visit('/static');
+
+  });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,9 +1,9 @@
 Cypress.Commands.add('addIncompleteItem', (itemValue) => {
 
   cy.get('[data-cy="new-item"]')
-    .type(itemValue)
-    .should('have.value',itemValue)
-    .trigger('keydown', { keyCode: 13, which: 13 });
+  .type(itemValue)
+  .should('have.value',itemValue)
+  .trigger('keydown', { keyCode: 13, which: 13 });
 
 });
 
@@ -12,10 +12,10 @@ Cypress.Commands.add('addCompleteItem', (itemValue) => {
   cy.addIncompleteItem(itemValue);
 
   cy.get('[data-cy="incomplete-list-holder"]')
-    .children()
-    .last()
-    .find('[data-cy="complete-me-checkbox"]')
-    .click({
-      force: true
-    });
+  .children()
+  .last()
+  .find('[data-cy="complete-me-checkbox"]')
+  .click({
+    force: true
+  });
 });
